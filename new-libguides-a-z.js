2471f515db2f09b17c56248f6f2410af1e406ea7
Nicholas E. Dease <56933343+ndease@users.noreply.github.com>
2024-03-11 11:58:28 -0400

    add js and new classes

---
+++ new-libguides-a-z.js
@@ -0,0 +1,8 @@
+<script>
+
+let bcNav = document.querySelector("#s-lib-bc");
+
+document.querySelector("header").append(bcNav);
+document.querySelector("#s-lib-bc").className = "container";
+
+</script>
